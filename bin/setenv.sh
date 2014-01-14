9bfec654ebe0e03fe72a21a50972c4bd9bfa4942
Alex Panchenko <alex.panchenko@gmail.com>
2014-01-14 17:46:16 +0700

    set CATALINA_PID also for shutdown.sh

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 CATALINA_OPTS="-Xmx1024m -XX:MaxPermSize=256m"
-if (echo "$_" | grep -q startup); then
+if (echo "$_" | grep -q startup\\\|shutdown); then
   CATALINA_PID=$CATALINA_BASE/work/tomcat.pid
 fi
